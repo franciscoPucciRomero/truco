889d24c6eda9a4b9c2948018d3f9bdbfee2b5292
franciscoPucciRomero <86837003+franciscoPucciRomero@users.noreply.github.com>
2022-05-27 12:27:12 -0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,137 @@
+//ejercicio:
+//autor:
+//fecha:
+//comentaro:
+/*
+1=espada
+2=basto
+3=oro
+4=copa
+*/
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    int cartaJ1[2]= {11 , 1};
+    int cartaJ2[2]= {2 , 1};
+    int cartaJ3[2]= {10 , 3};
+    int acumuladorTantos=0;
+
+    //cout<<cartaJ1[0];
+    // tantos
+    if(cartaJ1[1]==cartaJ2[1] || cartaJ1[1]==cartaJ3[1] ||cartaJ3[1]==cartaJ2[1])//hay tantos
+    {
+        acumuladorTantos=20;
+        //determinar mayor de tres
+        if(cartaJ1[1]==cartaJ2[1] && cartaJ1[1]==cartaJ3[1])
+        {
+            if(cartaJ1[0]<cartaJ2[0] && cartaJ1[0]<cartaJ3[0])
+            {
+
+                if(cartaJ3[0]<8)
+                {
+                    acumuladorTantos+=cartaJ3[0];
+                }
+                if(cartaJ2[0]<8)
+                {
+                    acumuladorTantos+=cartaJ2[0];
+                }
+            }
+            else{
+                if(cartaJ2[0]<cartaJ1[0] && cartaJ2[0]<cartaJ3[0])
+                {
+
+                        if(cartaJ1[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ1[0];
+                        }
+                        if(cartaJ3[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ3[0];
+                        }
+                }
+                else{
+
+                        if(cartaJ1[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ1[0];
+                        }
+                        if(cartaJ2[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ2[0];
+                        }
+                }
+            }
+        }
+        //solo dos cartas
+        else{
+            if(cartaJ1[1]==cartaJ2[1])
+            {
+
+                if(cartaJ1[0]<8)
+                {
+                    acumuladorTantos+=cartaJ1[0];
+                }
+                if(cartaJ2[0]<8)
+                {
+                    acumuladorTantos+=cartaJ2[0];
+                }
+            }
+            else
+                {
+                    if(cartaJ1[1]==cartaJ3[1])
+                    {
+
+                        if(cartaJ1[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ1[0];
+                        }
+                        if(cartaJ3[0]<8)
+                        {
+                            acumuladorTantos+=cartaJ3[0];
+                        }
+                    }
+                    else
+                        {
+                                if(cartaJ3[1]==cartaJ2[1])
+                                {
+
+                                    if(cartaJ3[0]<8)
+                                    {
+                                        acumuladorTantos+=cartaJ3[0];
+                                    }
+                                    if(cartaJ2[0]<8)
+                                    {
+                                        acumuladorTantos+=cartaJ2[0];
+                                    }
+                                }
+                        }
+                }
+            }
+    }
+    //mayor carta unica
+    else{
+        if( cartaJ1[0]>cartaJ2[0] && cartaJ1[0]>cartaJ3[0] )
+        {
+            acumuladorTantos=cartaJ1[0];
+        }
+        else{
+            if(cartaJ2[0]>cartaJ3[0])
+            {
+                acumuladorTantos=cartaJ2[0];
+            }
+            else{
+                acumuladorTantos=cartaJ3[0];
+            }
+        }
+    }
+    cout<<acumuladorTantos;
+
+
+
+
+
+return 0;
+}
---
+++ truco.cpp
@@ -0,0 +1,85 @@
+//ejercicio:
+//autor:
+//fecha:
+//comentaro:
+/*
+1=espada
+2=basto
+3=oro
+4=copa
+
+*/
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+void repartir(int *c1,int *c2,int *c3, int *c4, int *c5, int *c6)
+{
+
+}
+void turno1()
+{
+    int eleccion;
+    if(mano==0)
+    {
+        cout<<"precione 1 para embido, 2 para truco, 3 para pasar";
+        cin>>eleccion;
+    }
+    else
+        {
+            if(pedirtantos)
+            {
+
+            }
+
+
+        }
+
+
+
+}
+void enunciarMano()
+{
+
+}
+int pedirtantos()
+{
+
+}
+int tantos()
+{
+}
+int main()
+{
+    int cartaJ1[2];
+    int cartaJ2[2];
+    int cartaJ3[2];
+    //
+    int cartaPC1[2];
+    int cartaPC2[2];
+    int cartaPC3[2];
+    int mano=0;
+    int puntosPC=0;
+    int puntosJ=0;
+    //
+
+    while(puntosJ<30 || puntosPC<30)
+    {
+        repartir( &cartaJ1 , &cartaJ2 , &cartaJ3 , &cartaPC1 , &cartaPC2 , &cartaPC3 );
+
+        enunciarMano();
+
+        turno1();
+
+
+
+    }
+
+
+
+
+
+
+
+
+return 0;
+}
